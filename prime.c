363ea31fe427ff01eebb0c330c5823800b75a8d4
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-08-07 00:07:00 +0530

    fixed prime number code

--- prime.c
+++ prime.c
@@ -1,24 +1,32 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
-int main() {
-    int i,n = 11; // number to checked
-    int isPrime = 0;
-    for (i = 2;i < sqrt(11); i += 1) {
-        if (i % n == 0) {
-            continue;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Prime number is not passed");
+        return 0;
+    }
+
+    int i, isPrime = 1, n = atoi(argv[1]); // number to checked
+    int limit = fabs(sqrt(n));
+
+    for (i = 2; i < limit; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
         }
-        isPrime = 1;
     }
 
     printf("\n\n");
 
     if (isPrime) {
         printf("%d is prime", n);
-    } else {
+    }
+    else {
         printf("%d is not a prime", n);
     }
-    
+
     printf("\n\n");
     return 0;
 }
